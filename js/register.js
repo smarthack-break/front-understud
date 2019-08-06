90df3fdc5546df434026907ab805af1914ebb1f6
ChoiHyeongu <a11dsd123@gmail.com>
2019-08-07 04:16:28 +0900

    choi

---
+++ js/register.js
@@ -0,0 +1,12 @@
+function eraseLicense() {
+
+  alert("Click");
+
+  var nameText = document.getElementById('name');
+  var classText = document.getElementById('class');
+  var jobText = document.getElementById('job');
+
+  nameText.value = '';
+  classText.value = '';
+  jobText.value = '';
+}
